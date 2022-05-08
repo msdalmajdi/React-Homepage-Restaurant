6056ba38883d892a99e47b23aa893f521392341e
Msdalmajdi <msdalmajdi@hotmail.com>
2022-05-08 20:35:02 +0300

    FINISHED Task 2 at last

--- src/products.js
+++ src/products.js
@@ -4,6 +4,8 @@ const products = [
   { name: "Burger", price: "$3.5", image: "./burger.jpg" },
   { name: "Pasta", price: "$4.5", image: "./pasta.jpg" },
   { name: "Pizza", price: "$9.5", image: "./pizza.jpg" },
+  { name: "Pizza", price: "$9.5", image: "./pizza.jpg" },
+  { name: "Pizza", price: "$9.5", image: "./pizza.jpg" },
 ];
 //{ name: "Pizza", price: "$9.5", image: "./pizza.jpg" }
 const productsList = products.map((product) => {
