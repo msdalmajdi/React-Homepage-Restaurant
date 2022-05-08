532d2d2d7bc59a3aebf5e7c3001812bb557c0bef
Msdalmajdi <msdalmajdi@hotmail.com>
2022-05-08 18:52:03 +0300

    FINISHED according to task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
+    <div class="container">
+      <div class="title">
+        <h1>Nice Restaurant Somewhere</h1>
+      </div>
+
+      <div class="description">
+        <p>
+          This is a restaurant that is supposed to be located somehwere in
+          london it offers a variety of dishes that you should try.
+        </p>
+      </div>
+
+      <div class="image">
+        <img class="imagehero" src="./london-stock.jpg"></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +42,5 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+  
+    */
